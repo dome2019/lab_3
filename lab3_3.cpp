d6b5fcefdc6e34a85e73898e55f55978af7618fe
lab303-8 <lab303-8@labeng.local>
2019-01-11 17:21:32 +0700

    lab3

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std ;
 int main(){
+    double x=2,y=0,q=69;
+    while(1/x>=1/q)
+    { 
+        y=y+(1/x);
+        x=x+1;
+        
 
-    cout << "...";
+    }
 
-    return 0 ;
-
+    cout<<y;
+return 0  ;
 }
